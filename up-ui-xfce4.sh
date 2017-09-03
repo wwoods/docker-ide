937992a546b0e0fd5a876ac03e8ac6f705a40c65
Walt Woods <woodswalben@gmail.com>
2017-09-03 11:39:06 -0700

    Refactored UI code, based on ubuntu 17.04 to work around systemd issue

---
+++ up-ui-xfce4.sh
@@ -0,0 +1,10 @@
+# Example script that launches the ide-ui container
+
+export XEPH_DISPLAY=":10"
+Xephyr $XEPH_DISPLAY -ac -br -fullscreen -glamor &
+BLAH=$!
+export DISPLAY="$XEPH_DISPLAY"
+echo "TODO: nvidia-docker-compose"
+docker-compose rm -f && docker-compose build && docker run --rm --device /dev/snd -e DISPLAY --cap-add sys_admin -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1 $* ide-ui-xfce4
+kill $BLAH
+
