5f6e8d6c3d06b94077d953761602c63cea570445
Walt Woods <woodswalben@gmail.com>
2018-11-01 18:46:58 -0700

    Using x11docker for more trustworthy GPU / Xephyr usage

--- up-ui-xfce4.sh
+++ up-ui-xfce4.sh
@@ -1,10 +1,14 @@
 # Example script that launches the ide-ui container
 
-export XEPH_DISPLAY=":10"
-Xephyr $XEPH_DISPLAY -ac -br -fullscreen -glamor &
-BLAH=$!
-export DISPLAY="$XEPH_DISPLAY"
-echo "TODO: nvidia-docker-compose"
-docker-compose rm -f && docker-compose build ide-ui-xfce4 && docker run --rm --device /dev/snd -e DISPLAY --cap-add sys_admin -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1 $* ide-ui-xfce4
-kill $BLAH
+#export XEPH_DISPLAY=":10"
+#Xephyr $XEPH_DISPLAY -ac -br -fullscreen -glamor &
+#BLAH=$!
+#export DISPLAY="$XEPH_DISPLAY"
+#echo "TODO: nvidia-docker-compose"
+GPU=no
+GPU_ARGS="--gpu --hostdisplay --xorg"
+[ "$GPU" != "yes" ] && GPU_ARGS=""
+docker-compose rm -f && docker-compose build ide-ui-xfce4 && x11docker/x11docker ${GPU_ARGS} --desktop -fy --no-init --runasroot "rm -rf /fakehome/${USER}; ln -s /home/dock /fakehome/${USER}" -- --rm -- ide-ui-xfce4 xfce4-session
+#docker run --rm --device /dev/snd -e DISPLAY --cap-add sys_admin -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /usr/lib/x86_64-linux-gnu/libXv.so.1:/usr/lib/x86_64-linux-gnu/libXv.so.1 $* ide-ui-xfce4
+#kill $BLAH
 
